dcc7b39489c02feac97485e541a9110420169598
jinziqing1492 <726820352@qq.com>
2023-09-14 17:07:19 +0800

    web_demo页面简易实现

--- web_demo.py
+++ web_demo.py
@@ -1,41 +1,100 @@
-# import gradio as gr
-
-# filevisle = True
-
-# with gr.Blocks() as demo:
-#     gr.HTML("""<h1 align="center">MemoryGPT</h1>""")
-
-#     chatbot = gr.Chatbot()
-#     with gr.Row():
-#         input_file = gr.File()
-
-#         def hidupload(file):
-#             input_file.hide()
-#         input_file.upload(hidupload)
-#     # submitBtn.click(predict, [user_input, chatbot, max_length, top_p, temperature, history, past_key_values],
-#     #                 [chatbot, history, past_key_values], show_progress=True)
-#     # submitBtn.click(reset_user_input, [], [user_input])
-
-#     # emptyBtn.click(reset_state, outputs=[chatbot, history, past_key_values], show_progress=True)
-
-# demo.queue().launch(share=False, inbrowser=True)
 import gradio as gr
+import mdtex2html
 
-def predict_text(text):
-    # 在这里进行文本处理和预测
-    return "预测结果：" + text
+"""Override Chatbot.postprocess"""
 
-# 创建一个文本输入框
-text_input = gr.Textbox()
-# 创建一个按钮组件
-button = gr.Button("隐藏输入框")
 
-# 当按钮被点击时，隐藏输入框
-def hide_text_input():
-    text_input.hide()
+def postprocess(self, y):
+    if y is None:
+        return []
+    for i, (message, response) in enumerate(y):
+        y[i] = (
+            None if message is None else mdtex2html.convert((message)),
+            None if response is None else mdtex2html.convert(response),
+        )
+    return y
 
-button.click(hide_text_input)
 
-# 创建 Gradio 接口
-iface = gr.Interface(fn=predict_text, inputs=text_input, outputs="text")
-iface.launch()
+gr.Chatbot.postprocess = postprocess
+
+
+def parse_text(text):
+    """copy from https://github.com/GaiZhenbiao/ChuanhuChatGPT/"""
+    lines = text.split("\n")
+    lines = [line for line in lines if line != ""]
+    count = 0
+    for i, line in enumerate(lines):
+        if "```" in line:
+            count += 1
+            items = line.split('`')
+            if count % 2 == 1:
+                lines[i] = f'<pre><code class="language-{items[-1]}">'
+            else:
+                lines[i] = f'<br></code></pre>'
+        else:
+            if i > 0:
+                if count % 2 == 1:
+                    line = line.replace("`", "\`")
+                    line = line.replace("<", "&lt;")
+                    line = line.replace(">", "&gt;")
+                    line = line.replace(" ", "&nbsp;")
+                    line = line.replace("*", "&ast;")
+                    line = line.replace("_", "&lowbar;")
+                    line = line.replace("-", "&#45;")
+                    line = line.replace(".", "&#46;")
+                    line = line.replace("!", "&#33;")
+                    line = line.replace("(", "&#40;")
+                    line = line.replace(")", "&#41;")
+                    line = line.replace("$", "&#36;")
+                lines[i] = "<br>"+line
+    text = "".join(lines)
+    return text
+
+
+def predict(input, chatbot, max_length, top_p, temperature, history, past_key_values):
+    chatbot.append((parse_text(input), ""))
+    chatbot[-1] = (parse_text(input), parse_text(input))
+
+    yield chatbot, history, past_key_values
+
+
+def reset_user_input():
+    return gr.update(value='')
+
+
+def reset_state():
+    return [], [], None
+
+
+with gr.Blocks() as demo:
+    gr.HTML("""<h1 align="center">ChatGLM2-6B</h1>""")
+
+    chatbot = gr.Chatbot()
+    with gr.Row():
+        with gr.Column(scale=4):
+            with gr.Column(scale=12):
+                user_input = gr.Textbox(show_label=False, placeholder="Input...", lines=10).style(
+                    container=False)
+            with gr.Column(min_width=32, scale=1):
+                submitBtn = gr.Button("Submit", variant="primary")
+        with gr.Column(scale=1):
+            emptyBtn = gr.Button("Clear History")
+            max_length = gr.Slider(0, 32768, value=8192, step=1.0, label="Maximum length", interactive=True)
+            top_p = gr.Slider(0, 1, value=0.8, step=0.01, label="Top P", interactive=True)
+            temperature = gr.Slider(0, 1, value=0.95, step=0.01, label="Temperature", interactive=True)
+
+    history = gr.State([])
+    past_key_values = gr.State(None)
+
+    user_input.submit(predict, [user_input, chatbot, max_length, top_p, temperature, history, past_key_values],
+                    [chatbot, history, past_key_values], show_progress=True)
+    user_input.submit(reset_user_input, [], [user_input])
+    
+    submitBtn.click(predict, [user_input, chatbot, max_length, top_p, temperature, history, past_key_values],
+                    [chatbot, history, past_key_values], show_progress=True)
+    submitBtn.click(reset_user_input, [], [user_input])
+
+    emptyBtn.click(reset_state, outputs=[chatbot, history, past_key_values], show_progress=True)
+
+
+demo.queue().launch(share=False, inbrowser=True)
